d4a282507933f1f4bbd76a1da66b94e8b9ff9a92
hanshino <c0952199517@gmail.com>
2021-10-03 19:24:35 +0800

    移除壞壞的套件

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,4 @@ module.exports = {
   moduleFileExtensions: ["js"],
   transformIgnorePatterns: ["/node_modules/"],
   testMatch: ["<rootDir>/**/__tests__/**/*.js", "<rootDir>/**/*.{spec,test}.js"],
-  setupFilesAfterEnv: ["./jest.setup.redis-mock.js"],
 };
