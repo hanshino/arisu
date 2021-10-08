4584685dc7c2405b45dbd7bf661fb240ba93dfaf
cameron.hsieh <cameron.hsieh@coolbe.com.tw>
2021-10-08 15:29:28 +0800

    fix CI flow

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const rateLimiter = require("./middlewares/rateLimiter");
 const apiRouter = require("./routes/api");
 const expressRespond = require("../lib/express-respond");
 
+if (!process.env.PROJECT_NAME) {
+  require("dotenv").config();
+}
+
 const app = express();
 app.use(expressRespond);
 app.use("/api/", rateLimiter, express.json(), apiRouter);
